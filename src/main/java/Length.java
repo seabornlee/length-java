704ade29ef96864f6f0389bc2b08b8daa8aaef80
Linesh <linesh.simpcity@gmail.com>
2019-11-04 15:21:46 +0800

    refactor: unitify getUnit() v: rename currentUnit back to unit

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,16 +1,16 @@
 public class Length {
     private final double value;
-    private final Units currentUnit;
+    private final Units unit;
 
     public Length(double value, String unit) {
         this.value = value;
-        this.currentUnit = Units.valueOf(unit.toUpperCase());
+        this.unit = Units.valueOf(unit.toUpperCase());
     }
 
     public Length as(String unit) {
         Length length = this;
         Units targetUnit = Units.valueOf(unit.toUpperCase());
-        if (this.currentUnit == Units.F) {
+        if (this.unit == Units.F) {
             if (targetUnit == Units.YARD) {
                 length = new Length(this.value / 3, unit);
             } else if (targetUnit == Units.INCH) {
@@ -18,7 +18,7 @@ public class Length {
             }
         }
 
-        if (this.currentUnit == Units.YARD) {
+        if (this.unit == Units.YARD) {
             if (targetUnit == Units.INCH) {
                 length = new Length(this.value * 36, unit);
             } else if (targetUnit == Units.F){
@@ -26,7 +26,7 @@ public class Length {
             }
         }
 
-        if (this.currentUnit == Units.INCH) {
+        if (this.unit == Units.INCH) {
             if (targetUnit == Units.F) {
                 length = new Length(this.value / 12, unit);
             } else if (targetUnit == Units.YARD) {
@@ -42,6 +42,6 @@ public class Length {
     }
 
     public Units getUnit() {
-        return this.currentUnit;
+        return this.unit;
     }
 }
