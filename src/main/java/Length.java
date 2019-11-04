94f1e52cb3c7643e62a0eb27367e4bd13b7a1878
Linesh <linesh.simpcity@gmail.com>
2019-11-04 16:12:52 +0800

    refactor: step vi: remove all other references

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -9,31 +9,25 @@ public class Length {
 
     public Length as(Units targetUnit) {
         double value = this.value;
-        Length length = new Length(value, targetUnit);
         if (this.unit == Units.FOOT) {
             if (targetUnit == Units.YARD) {
                 value = this.value / 3;
-                length = new Length(value, targetUnit);
             } else if (targetUnit == Units.INCH) {
                 value = this.value * 12;
-                length = new Length(value, targetUnit);
             }
         }
 
         if (this.unit == Units.YARD) {
             if (targetUnit == Units.INCH) {
                 value = this.value * 36;
-                length = new Length(value, targetUnit);
             } else if (targetUnit == Units.FOOT){
                 value = this.value * 3;
-                length = new Length(value, targetUnit);
             }
         }
 
         if (this.unit == Units.INCH) {
             if (targetUnit == Units.FOOT) {
                 value = this.value / 12;
-                length = new Length(value, targetUnit);
             } else if (targetUnit == Units.YARD) {
                 value = this.value / 36;
             }
