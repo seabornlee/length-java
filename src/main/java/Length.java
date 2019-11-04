24ba12f22227443291632c6d00942aa204e979a0
Linesh <linesh.simpcity@gmail.com>
2019-11-04 15:26:52 +0800

    refactor: unitify constructor iv: replace rest other references

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -16,25 +16,25 @@ public class Length {
         Units targetUnit = Units.valueOf(unit.toUpperCase());
         if (this.unit == Units.F) {
             if (targetUnit == Units.YARD) {
-                length = new Length(this.value / 3, unit);
+                length = new Length(this.value / 3, targetUnit);
             } else if (targetUnit == Units.INCH) {
-                length = new Length(this.value * 12, unit);
+                length = new Length(this.value * 12, targetUnit);
             }
         }
 
         if (this.unit == Units.YARD) {
             if (targetUnit == Units.INCH) {
-                length = new Length(this.value * 36, unit);
+                length = new Length(this.value * 36, targetUnit);
             } else if (targetUnit == Units.F){
-                length = new Length(this.value * 3, unit);
+                length = new Length(this.value * 3, targetUnit);
             }
         }
 
         if (this.unit == Units.INCH) {
             if (targetUnit == Units.F) {
-                length = new Length(this.value / 12, unit);
+                length = new Length(this.value / 12, targetUnit);
             } else if (targetUnit == Units.YARD) {
-                length = new Length(this.value / 36, unit);
+                length = new Length(this.value / 36, targetUnit);
             }
         }
 
